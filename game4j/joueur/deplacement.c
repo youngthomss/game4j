5cdada5c1fc27b8b825aa894ab4ecd079770a6d1
Julian <akueson85@gmail.com>
2022-12-07 21:07:35 +0100

    [feat]gere les diagonales

--- game4j/joueur/deplacement.c
+++ game4j/joueur/deplacement.c
@@ -8,7 +8,11 @@ enum direction {
   PAVE_HAUT = '8',
   PAVE_DROIT = '6',
   PAVE_BAS = '2',
-  PAVE_GAUCHE = '4'
+  PAVE_GAUCHE = '4',
+  PAVE_HAUT_DROIT = '9',
+  PAVE_HAUT_GAUCHE = '7',
+  PAVE_BAS_DROIT = '3',
+  PAVE_BAS_GAUCHE = '1'
 };
 // parametre la taille de la fenetre
 #define LONGUEUR 25
@@ -16,12 +20,6 @@ enum direction {
 #define X 10
 #define Y 10
 
-// En incluant la bibliothèque ncurses.h, nous n'avons plus besoin d'inclure la
-// bibliothèque stdio.h car celle ci est inclue implicitement dans ncurses.h
-
-// Lors de la compilation avec gcc, il faut spécifier que vus utilisez la
-// bibliothèque Ncurses et pour cela il vous faut rajouter l'option lncurses.
-// Exemple gcc -lncurses -o HelloWorld HelloWorld.c
 
 void initStructureFenetre() {
   initscr(); // Initialise la structure WINDOW et autres paramètres
@@ -41,7 +39,7 @@ WINDOW *creerFenetre(int longueur, int largeur, int x, int y) {
 
 void redessinerFenetre(WINDOW *fenetre) {
   werase(fenetre);
-  box(fenetre, 0, 0); // recree la partie de la bordure droite supp
+  box(fenetre, 0, 0); 
   wrefresh(fenetre);
 }
 
@@ -72,6 +70,30 @@ void traiterDeplacement(WINDOW *fenetre, int input) {
     wrefresh(fenetre);
     break;
 
+case PAVE_HAUT_DROIT:
+    redessinerFenetre(fenetre);
+    mvwprintw(fenetre, 10, 25, "Tu te déplaces en haut à droite");
+    wrefresh(fenetre);
+    break;
+
+    case PAVE_HAUT_GAUCHE:
+    redessinerFenetre(fenetre);
+    mvwprintw(fenetre, 10, 25, "Tu te déplaces en haut à gauche");
+    wrefresh(fenetre);
+    break;
+
+    case PAVE_BAS_DROIT:
+    redessinerFenetre(fenetre);
+    mvwprintw(fenetre, 10, 25, "Tu te déplaces en bas à droite ");
+    wrefresh(fenetre);
+    break;
+
+    case PAVE_BAS_GAUCHE:
+    redessinerFenetre(fenetre);
+    mvwprintw(fenetre, 10, 25, "Tu te déplaces en bas à gauche");
+    wrefresh(fenetre);
+    break;
+
   default:
     redessinerFenetre(fenetre);
     mvwprintw(fenetre, 10, 15, "Erreur, choisissez une bonne direction");
@@ -84,9 +106,8 @@ int main() {
 
   initStructureFenetre();
   WINDOW *fenetre = creerFenetre(LONGUEUR, LARGEUR, X, Y);
-  keypad(fenetre, TRUE); /* pour récupérer les événements du clavier */
-  // nodelay(fenetre, TRUE); //pour eviter de bloquer la fenetre en attendant
-  // qu'une touche sois entrée
+  keypad(fenetre, TRUE);   
+  // nodelay(fenetre, TRUE); //pour eviter de bloquer la fenetre en attendant qu'une touche sois entrée
 
   int i = 0;
   while (i == 0) {
