70f49d1753a6632a65a2ca2b7ae4dfe08e2edfa5
ThibaulZGM <10thibaultz@gmail.com>
2022-12-28 16:53:12 +0100

    [feat] modif sauvergarde

--- include/sauvegarder.h
+++ include/sauvegarder.h
@@ -1,15 +1,23 @@
+#pragma once
 #include <stdio.h>
 #include <stdlib.h>
 #include "generation.h"
 #include "section.h"
-typedef struct Partie partie;
-typedef struct Partie* ptrPartie;
+#include "arbre.h"
 #define taillechemin 100
 #define taillenom 30
+typedef struct Partie partie;
+typedef struct Partie* ptrPartie;
+typedef struct Data data;
+typedef struct Data* ptrData;
 
 struct Partie{
+    int id; 
     char chemin[taillechemin];
     char nomPartie[taillenom];
 };
 
-void CreerNomSauvegarde(char* chemin, char* nom);
+void NomSauvegarde(char* chemin);
+void nouvellePartie(char* nomPartie, char* chemin);
+void CreerFichierSauvegarde(partie info, ptrSection** carte, int** arbre, int taille);
+void ouvrirFichier(partie info, ptrSection** carte, int** arbre, int taille);
